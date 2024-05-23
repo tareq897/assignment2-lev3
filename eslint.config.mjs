28f3610887410c0a34f7e2cb4bc6ed6b9b1a49cd
tareq897 <itareqislam554@gmail.com>
2024-05-23 23:02:19 +0600

    multiple-error solved

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ export default tseslint.config(
       "no-undef": "error",
       "prefer-const": "error",
       "no-console": "warn",
+      "@typescript-eslint/no-explicit-any": "off"
     },
   },
   {
